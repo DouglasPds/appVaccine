f72586cc3ec640eb67bd9bb0c15e5caa05e5fa46
DouglasPds <douglas17dd@hotmail.com>
2021-10-15 20:44:25 -0300

    refactor: use component ShowDate, the array of dates in json and create his style

---
+++ src/screens/DateVacine/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FlatList } from 'react-native'
+import { ShowDate } from '../../components/ShowDate'
+
+import * as Date from '../../json/DateValues.json'
+
+import { Container, Title } from './styles'
+const { Dates } = Date
+
+export const DateVacine: React.FC = () => {
+  return (
+		<Container>
+			<Title>Datas de vacinação</Title>
+			<FlatList
+				data={Dates}
+				keyExtractor={item => item.id}
+				renderItem={({ item }) => (
+					<ShowDate dateProps={item} />
+				)}
+			/>
+		</Container>
+  )
+}
---
+++ src/screens/DateVacine/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+	align-items: center;
+`
+export const Title = styled.Text`
+	margin: 20px 0;
+	font-size: 30px;
+`
