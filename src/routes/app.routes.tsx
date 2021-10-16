6b3a5c5ebc0d34e2c631564b114c3341282094c6
DouglasPds <douglas17dd@hotmail.com>
2021-10-15 21:51:43 -0300

    refactor: use icons

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FontAwesome, MaterialIcons, Fontisto } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { DateVaccine } from '../screens/DateVaccine'
 import { VaccinationCard } from '../screens/VaccinationCard'
+import { VaccinationCardPets } from '../screens/VaccinationCardPets'
+import { Account } from '../screens/Account'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
@@ -12,11 +15,53 @@ export const AppRoutes = () => {
 			<Screen
 				name="Vacinação"
 				component={DateVaccine}
+				options={{
+				  tabBarIcon: ({ size }) => (
+						<FontAwesome
+							name="calendar"
+							size={size}
+						/>
+				  )
+				}}
 			/>
 
 			<Screen
 				name="Histórico"
 				component={VaccinationCard}
+				options={{
+				  tabBarIcon: ({ size }) => (
+						<Fontisto
+							name="injection-syringe"
+							size={size}
+						/>
+				  )
+				}}
+			/>
+
+			<Screen
+				name="Histórico Pets"
+				component={VaccinationCardPets}
+				options={{
+				  tabBarIcon: ({ size }) => (
+						<MaterialIcons
+							name="pets"
+							size={size}
+						/>
+				  )
+				}}
+			/>
+
+			<Screen
+				name="Minha conta"
+				component={Account}
+				options={{
+				  tabBarIcon: ({ size }) => (
+						<MaterialIcons
+							name="account-circle"
+							size={size}
+						/>
+				  )
+				}}
 			/>
 		</Navigator>
   )
